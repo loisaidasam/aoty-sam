9d910aa78364ebf90fbcd96d2b90a6dd00dd540d
LoisaidaSam <sam.sandberg@gmail.com>
2018-12-04 15:13:25 -0500

    Move `spotify_redirect_uri` into `.env`

--- gettoken.py
+++ gettoken.py
@@ -10,10 +10,10 @@ import spotipy.util as util
 
 
 SCOPE = 'user-library-read'
-REDIRECT_URI = 'http://localhost'
 USERNAME = os.environ.get('spotify_user')
 CLIENT_ID = os.environ.get('spotify_client_id')
 CLIENT_SECRET = os.environ.get('spotify_client_secret')
+REDIRECT_URI = os.environ.get('spotify_redirect_uri')
 
 print util.prompt_for_user_token(USERNAME,
                                  scope=SCOPE,
